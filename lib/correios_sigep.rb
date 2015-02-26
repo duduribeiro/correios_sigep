ed848eef3f61f3fa80ca137bb2f5419aa6bd97ee
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-02-26 15:51:23 -0300

    Add require to the main class

--- lib/correios_sigep.rb
+++ lib/correios_sigep.rb
@@ -1,5 +1,14 @@
 require 'correios_sigep/version'
 require 'correios_sigep/configuration'
+require 'correios_sigep/models/recipient'
+require 'correios_sigep/models/product'
+require 'correios_sigep/models/recipient'
+require 'correios_sigep/models/sender'
+require 'correios_sigep/models/object'
+require 'correios_sigep/models/sender'
+require 'correios_sigep/models/collect'
+require 'correios_sigep/models/logistic_reverse'
+require 'nokogiri'
 
 module CorreiosSigep
   def self.configuration
