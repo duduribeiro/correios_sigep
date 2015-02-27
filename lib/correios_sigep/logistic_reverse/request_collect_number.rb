01f8ac647d53ec2b8ebae5a737cc40050aa39ddb
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-02-27 18:46:06 -0300

    remove pry from class

--- lib/correios_sigep/logistic_reverse/request_collect_number.rb
+++ lib/correios_sigep/logistic_reverse/request_collect_number.rb
@@ -1,4 +1,3 @@
-require 'pry'
 module CorreiosSigep
   module LogisticReverse
     class RequestCollectNumber < BaseClient
--- lib/correios_sigep/models/logistic_reverse.rb
+++ lib/correios_sigep/models/logistic_reverse.rb
@@ -1,4 +1,3 @@
-require 'pry'
 module CorreiosSigep
   module Models
     class LogisticReverse
