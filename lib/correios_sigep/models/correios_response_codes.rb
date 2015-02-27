c2368892e234975a1cdb45fbd6b1b0c0925496f3
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-02-27 14:49:56 -0300

    Create the class wich correios return codes

---
+++ lib/correios_sigep/models/correios_response_codes.rb
@@ -0,0 +1,12 @@
+module CorreiosSigep
+  module Models
+    module CorreiosResponseCodes
+      SUCCESS                               = 0
+      TICKET_ALREADY_USED                   = 195
+      UNAVAILABLE_SERVICE                   = 210
+      INEXISTENT_ZIPCODE                    = 113
+      UNAVAILABLE_HOUSE_COLLECT             = 111
+      COLLECT_NOT_ANSWERED_FOR_THE_ZIPCODE  = 117
+    end
+  end
+end
