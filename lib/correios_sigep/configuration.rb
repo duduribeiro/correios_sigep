237c88631c5fb38de351b8d691c22d45187f482f
Felipe Gentil <cdigentil@gmail.com>
2015-04-24 10:13:13 -0300

    Add possibility to pass a proxy

--- lib/correios_sigep/configuration.rb
+++ lib/correios_sigep/configuration.rb
@@ -1,6 +1,6 @@
 module CorreiosSigep
   class Configuration
     attr_accessor :administrative_code, :card, :contract, :password,
-                  :service_code, :user, :wsdl_base_url
+                  :service_code, :user, :wsdl_base_url, :proxy
   end
 end
