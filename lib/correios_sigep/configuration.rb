d71fb2db552e12acf1571965f2c2c4031c4fee2d
Carlos Ribeiro <duduribeiro.gba@gmail.com>
2015-06-18 16:57:02 -0300

    Create a model Administrative Fields to receive this fields for each logistic reverse

--- lib/correios_sigep/configuration.rb
+++ lib/correios_sigep/configuration.rb
@@ -2,5 +2,13 @@ module CorreiosSigep
   class Configuration
     attr_accessor :administrative_code, :card, :contract, :password,
                   :service_code, :user, :wsdl_base_url, :proxy
+
+    def administrative_fields
+      @administrative_fields ||=
+        Models::AdministrativeFields.new(administrative_code: administrative_code,
+                                         card: card, contract: contract,
+                                         service_code: service_code)
+
+    end
   end
 end
