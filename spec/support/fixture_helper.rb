80d4cf7c8f066a3e0ac1813e1b07987404765686
Felipe Gentil <cdigentil@gmail.com>
2015-07-01 16:12:26 -0300

    Refactor request collect number specs

--- spec/support/fixture_helper.rb
+++ spec/support/fixture_helper.rb
@@ -6,4 +6,8 @@ module FixtureHelper
   def builders_fixture(filename)
     File.read("./spec/fixtures/builders/#{filename}")
   end
+
+  def request_fixture(filename)
+    File.read("./spec/fixtures/requests/#{filename}")
+  end
 end
