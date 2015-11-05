32de21ae5ee88b5bbfff3072443f55bba45f695f
Felipe Gentil <cdigentil@gmail.com>
2015-11-05 15:43:28 -0200

    Handle Correios response for SRO when object is an array

--- lib/correios_sigep/logistic_reverse/request_sro.rb
+++ lib/correios_sigep/logistic_reverse/request_sro.rb
@@ -15,14 +15,19 @@ module CorreiosSigep
       private
       def process_response response
         correios_hash = response.body[:acompanhar_pedido_response][:return]
-        check_sro_errors correios_hash
-        correios_hash[:coleta][:objeto][:numero_etiqueta] || begin raise Models::Errors::SRONotReady; end
+        check_sro_errors(correios_hash)
+        correios_response(correios_hash[:coleta][:objeto])[:numero_etiqueta] || begin raise Models::Errors::SRONotReady; end
+      end
+
+      def correios_response response
+        response.is_a?(Array) ? response.first : response
       end
 
       def check_sro_errors return_body
         error_msg = return_body[:msg_erro]
         case return_body[:cod_erro].to_i
         when 0
+          return
         when -7
           raise Models::Errors::RequiredFields.new(error_msg)
         when -5
