cc6d634007d5a1c342eac717f63a8de513c2dc61
Leandro Maduro <leandro.maduro@walmart.com>
2017-01-12 19:11:47 -0200

    Fix a typo

--- spec/correios_sigep/logistic_reverse/base_client_spec.rb
+++ spec/correios_sigep/logistic_reverse/base_client_spec.rb
@@ -48,14 +48,14 @@ module CorreiosSigep
         subject {  described_class.new.wsdl }
 
         context 'in a test environment' do
-          it 'return the staging url' do
+          it 'returns the staging url' do
             allow(ENV).to receive(:[]).with('GEM_ENV').and_return 'test'
             expect(subject).to eq 'http://webservicescolhomologacao.correios.com.br/ScolWeb/WebServiceScol?wsdl'
           end
         end
 
         context 'in other environment' do
-          it 'return the production url' do
+          it 'returns the production url' do
             allow(ENV).to receive(:[]).with('GEM_ENV').and_return 'prod'
             expect(subject).to eq 'http://webservicescol.correios.com.br/ScolWeb/WebServiceScol?wsdl'
           end
