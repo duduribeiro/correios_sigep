2cd5145dc2ea562037ef67cd1f1eb1a69095d49b
Leandro Maduro <leandro.maduro@walmart.com>
2017-01-12 19:17:50 -0200

    Fix tests in request_spec

--- spec/correios_sigep/builders/xml/request_spec.rb
+++ spec/correios_sigep/builders/xml/request_spec.rb
@@ -11,10 +11,7 @@ module CorreiosSigep
               expected_response = [
                 '<cartao>0057018901</cartao>',
                 '<codigo_servico>41076</codigo_servico>',
-                '<contrato>9912208555</contrato>',
-                '<codAdministrativo>08082650</codAdministrativo>',
-                '<senha>8o8otn</senha>',
-                '<usuario>60618043</usuario><test/>'
+                '<codAdministrativo>08082650</codAdministrativo><test/>'
               ].join + "\n"
               expect(described_class.build_xml request).to eq expected_response
             end
@@ -24,15 +21,11 @@ module CorreiosSigep
             it 'builds a Authentication XML with the override parameter' do
               administrative_fields = Models::AdministrativeFields.new(administrative_code: 'adm123',
                                                                        card: 'card123',
-                                                                       contract: 'cont123',
                                                                        service_code: 'ser123')
               expected_response = [
                 '<cartao>card123</cartao>',
                 '<codigo_servico>ser123</codigo_servico>',
-                '<contrato>cont123</contrato>',
-                '<codAdministrativo>adm123</codAdministrativo>',
-                '<senha>8o8otn</senha>',
-                '<usuario>60618043</usuario><test/>'
+                '<codAdministrativo>adm123</codAdministrativo><test/>'
               ].join + "\n"
               expect(described_class.build_xml request, administrative: administrative_fields).to eq expected_response
             end
