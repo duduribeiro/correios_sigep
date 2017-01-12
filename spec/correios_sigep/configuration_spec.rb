9ee027025257c978bae6a32378315f38e1d2ab50
Leandro Maduro <leandro.maduro@walmart.com>
2017-01-12 19:18:05 -0200

    Fix testes in configuration_spec

--- spec/correios_sigep/configuration_spec.rb
+++ spec/correios_sigep/configuration_spec.rb
@@ -28,33 +28,6 @@ module CorreiosSigep
       end
     end
 
-    describe '#contract' do
-      it 'returns nil when unset' do
-        expect(subject.contract).to be_nil
-      end
-    end
-
-    describe '#contract=' do
-      it 'sets the contract in configuration' do
-        subject.contract = 'contract'
-        expect(subject.contract).to eq 'contract'
-      end
-    end
-
-    describe '#password' do
-      it 'returns nil when unset' do
-        expect(subject.password).to be_nil
-      end
-    end
-
-    describe '#password=' do
-      it 'sets the password in configuration' do
-        subject.password = 'password'
-        expect(subject.password).to eq 'password'
-      end
-    end
-
-
     describe '#service_code' do
       it 'returns nil when unset' do
         expect(subject.service_code).to be_nil
@@ -68,19 +41,6 @@ module CorreiosSigep
       end
     end
 
-    describe '#user' do
-      it 'returns nil when unset' do
-        expect(subject.user).to be_nil
-      end
-    end
-
-    describe '#user=' do
-      it 'sets the user in configuration' do
-        subject.user = 'user'
-        expect(subject.user).to eq 'user'
-      end
-    end
-
     describe '#timeout' do
       it 'returns nil when unset' do
         expect(subject.timeout).to be_nil
@@ -91,6 +51,36 @@ module CorreiosSigep
       it 'sets the timeout in configuration' do
         subject.timeout = 15
         expect(subject.timeout).to eq 15
+      end
+    end
+
+    describe '#basic_auth_user' do
+      it 'returns nil when unset' do
+        expect(subject.basic_auth_user).to be_nil
+      end
+    end
+
+    describe '#basic_auth_user=' do
+      let(:user) { 'user' }
+
+      it 'sets the basic_auth_user in configuration' do
+        subject.basic_auth_user = user
+        expect(subject.basic_auth_user).to eq user
+      end
+    end
+
+    describe '#basic_auth_pass' do
+      it 'returns nil when unset' do
+        expect(subject.basic_auth_pass).to be_nil
+      end
+    end
+
+    describe '#basic_auth_pass=' do
+      let(:pass) { 'pass' }
+
+      it 'sets the basic_auth_pass in configuration' do
+        subject.basic_auth_pass = pass
+        expect(subject.basic_auth_pass).to eq pass
       end
     end
   end
